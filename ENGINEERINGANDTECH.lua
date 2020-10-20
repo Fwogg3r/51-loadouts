99304844a2bc98fb3ec3e27c85d9219d1bd2c9c9
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:34:57 -0400

    Create ENGINEERINGANDTECH.lua

---
+++ ENGINEERINGANDTECH.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5742159140, 5741893916}, HealthBonus = 0, TeamtagName = "Intern"},
+	[2] = {Tools = {"Glock 17", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5742159140, 5741893916}, HealthBonus = 5, TeamtagName = "Junior Technician"},
+	[3] = {Tools = {"Glock 17", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5741883667, 5741893916}, HealthBonus = 10, TeamtagName = "Technician"},
+	[4] = {Tools = {"Glock 17", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5741883667, 5741893916}, HealthBonus = 15, TeamtagName = "Engineer"},
+	[5] = {Tools = {"Glock 17", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5741883667, 5741893916}, HealthBonus = 20, TeamtagName = "Senior Engineer"},
+	[6] = {Tools = {"Glock 18", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5742159529, 5741893916}, HealthBonus = 25, TeamtagName = "Chief Engineer"},
+	[7] = {Tools = {"Glock 18", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5742159529, 5741893916}, HealthBonus = 30, TeamtagName = "Supervisor Technician"},
+	[8] = {Tools = {"Glock 18", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5742159529, 5741893916}, HealthBonus = 50, TeamtagName = "Head of Site Maintenance"},
+	[9] = {Tools = {"Glock 18", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5742159529, 5741893916}, HealthBonus = 75, TeamtagName = "Head Site Engineer"},
+	[10] = {Tools = {"P90", "Wrench", "Mop", "Torch"}, Morph = "", Clothing = {5742159529, 5741893916}, HealthBonus = 100, TeamtagName = "Overwatch of Site Management"},
+}
