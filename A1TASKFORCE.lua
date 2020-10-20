4b9d869e3b77ef309f8b48309d4778b195381b12
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:22:07 -0400

    Create A1TASKFORCE.lua

---
+++ A1TASKFORCE.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"SCAR-L", "MP5", "Glock 18", "Baton", "Bandage", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 25, TeamtagName = "Alpha-1 Auxilary"},
+	[2] = {Tools = {"SCAR-L", "MP5", "Glock 18", "Baton", "Bandage", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 50, TeamtagName = "Alpha-1 Operative"},
+	[3] = {Tools = {"SCAR-L", "Remington 870", "Glock 18", "Baton", "Bandage", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 75, TeamtagName = "Alpha-1 Corporal"},
+	[4] = {Tools = {"SCAR-L", "Remington 870", "Glock 18", "Baton", "Bandage", "Detain", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 100, TeamtagName = "Alpha-1 Sentinel"},
+	[5] = {Tools = {"SCAR-H", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 125, TeamtagName = "Alpha-1 Sentinel Corporal"},
+	[6] = {Tools = {"SCAR-H", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 150, TeamtagName = "Alpha-1 Sentinel Command"},
+	[7] = {Tools = {"G36", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 175, TeamtagName = "Alpha-1 Officer"},
+	[8] = {Tools = {"G36", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 200, TeamtagName = "Alpha-1 Lieutenant"},
+	[9] = {Tools = {"G36 Elite", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 225, TeamtagName = "Alpha-1 Praetor"},
+	[10] = {Tools = {"M60", "KSG 12", "Remington 700", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "Alpha-1", Clothing = {5743699581, 5743705429}, HealthBonus = 250, TeamtagName = "Alpha-1 Task Force Leader"},
+}
