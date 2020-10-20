71659374050c7e000ebe931c77b0fe6f0f01586b
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:33:59 -0400

    Create DISPOSABLECLASS.lua

---
+++ DISPOSABLECLASS.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Mop"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 0, TeamtagName = "Class Disposable"},
+	[2] = {Tools = {"Mop", "Torch"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 5, TeamtagName = "Class Disposable"},
+	[3] = {Tools = {"Mop", "Torch"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 10, TeamtagName = "Class Disposable"},
+	[4] = {Tools = {"Glock 17", "Mop", "Torch"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 15, TeamtagName = "Noteable Class Disposable"},
+	[5] = {Tools = {"Glock 17","Mop", "Torch"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 20, TeamtagName = "Noteable Class Disposable"},
+	[6] = {Tools = {"Glock 17","Mop", "Torch"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 25, TeamtagName = "Noteable Class Disposable"},
+	[7] = {Tools = {"Glock 18", "Mop", "Torch", "Bandage"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 30, TeamtagName = "Hazardous Class Disposable"},
+	[8] = {Tools = {"Glock 18", "Mop", "Torch", "Bandage"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 50, TeamtagName = "Hazardous Class Disposable"},
+	[9] = {Tools = {"Glock 18", "Mop", "Torch", "Bandage"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 75, TeamtagName = "Hazardous Class Disposable"},
+	[10] = {Tools = {"MP5","Glock 18", "Mop", "Bandage", "Torch"}, Morph = "", Clothing = {5743792929, 5743782039}, HealthBonus = 100, TeamtagName = "Class Disposable Union Member"},
+}
