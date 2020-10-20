cc3ce785765d7732e53be1115fea662440253eb4
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:31:13 -0400

    Create CHAOSINSURGENCY.lua

---
+++ CHAOSINSURGENCY.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"AK-12", "Glock 17", "Bandage", "Torch", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 25, TeamtagName = "Auxilary"},
+	[2] = {Tools = {"AK-12", "Glock 17", "Bandage", "Torch", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 50, TeamtagName = "Conscript"},
+	[3] = {Tools = {"AK-12", "Glock 17", "Bandage", "Torch", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 75, TeamtagName = "Recruit"},
+	[4] = {Tools = {"AK-12", "Glock 18", "Bandage", "Torch", "Medkit", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 100, TeamtagName = "Operative"},
+	[5] = {Tools = {"AK-12", "Glock 18", "Bandage", "Torch", "Medkit", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 125, TeamtagName = "Corporal"},
+	[6] = {Tools = {"AK-12", "Remington 870","Glock 18", "Bandage", "Torch", "Medkit", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 150, TeamtagName = "Sergeant"},
+	[7] = {Tools = {"AK-12", "Remington 870","Glock 18", "Bandage", "Torch", "Medkit", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 175, TeamtagName = "Operations Corporal"},
+	[8] = {Tools = {"AK-12", "Remington 700","Glock 18", "Bandage", "Torch", "Medkit", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 200, TeamtagName = "Operations Sentinel"},
+	[9] = {Tools = {"RPK", "Remington 700","Glock 18", "Bandage", "Torch", "Medkit", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 225, TeamtagName = "Operations Officer"},
+	[10] = {Tools = {"RPK", "Remington 700","Glock 18", "Bandage", "Torch", "Medkit", "Mop"}, Morph = "Conscript+", Clothing = {5743692340, 5743692927}, HealthBonus = 250, TeamtagName = "Operations Leading Officer"},
+}
