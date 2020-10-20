b969647ecddebb9c85a6b58f66bf2b78c51cf1a5
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:32:16 -0400

    Create EXTERNALINTERESTS.lua

---
+++ EXTERNALINTERESTS.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Torch", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 0, TeamtagName = "Intern"},
+	[2] = {Tools = {"Bandage", "Torch", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 5, TeamtagName = "Staff"},
+	[3] = {Tools = {"Bandage", "Torch", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 10, TeamtagName = "Resource Department"},
+	[4] = {Tools = {"MP5K", "Bandage", "Torch", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 15, TeamtagName = "External Commissions Department"},
+	[5] = {Tools = {"MP5K", "Bandage", "Torch", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 20, TeamtagName = "Executive Department"},
+	[6] = {Tools = {"MP5K", "Bandage", "Torch", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 25, TeamtagName = "Senior Staff"},
+	[7] = {Tools = {"MP5", "Bandage", "Torch", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 30, TeamtagName = "Board of Interests"},
+	[8] = {Tools = {"MP5", "Bandage", "Torch", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 50, TeamtagName = "Senior Management"},
+	[9] = {Tools = {"MP5", "Bandage", "Torch", "Mop", "Medkit", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 75, TeamtagName = "Secretary"},
+	[10] = {Tools = {"Uncivilous", "Bandage", "Torch", "Medkit", "Mop"}, Morph = "", Clothing = {5743685399, 5743783439}, HealthBonus = 100, TeamtagName = "Head of DoI"},
+}
