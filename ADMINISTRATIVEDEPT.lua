6fddeedcde70c3d62ee23c915264160c6454f34b
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:20:47 -0400

    Create ADMINISTRATIVEDEPT.lua

---
+++ ADMINISTRATIVEDEPT.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Level 5 Keycard", "MP5", "Glock 18", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743694915, 5743710206}, HealthBonus = 25, TeamtagName = "Junior Executive"},
+	[2] = {Tools = {"Level 5 Keycard", "MP5", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743694915, 5743710206}, HealthBonus = 50, TeamtagName = "Executive Personnel"},
+	[3] = {Tools = {"Level 5 Keycard", "P90", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743694915, 5743710206}, HealthBonus = 60, TeamtagName = "Executive Board Member"},
+	[4] = {Tools = {"Level 5 Keycard", "P90", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743695427, 5743710206}, HealthBonus = 75, TeamtagName = "Administrative Attendent"},
+	[5] = {Tools = {"Level 5 Keycard", "P90", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743695427, 5743710206}, HealthBonus = 100, TeamtagName = "Administrative Supervisor"},
+	[6] = {Tools = {"Level 5 Keycard", "P90", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743695427, 5743710206}, HealthBonus = 125, TeamtagName = "Administrative Manager"},
+	[7] = {Tools = {"Level 5 Keycard", "P90", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5741882264, 5743709556}, HealthBonus = 150, TeamtagName = "Administrative Council"},
+	[8] = {Tools = {"Level 5 Keycard", "P90", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5741882264, 5743709556}, HealthBonus = 175, TeamtagName = "Administrative Overwatch"},
+	[9] = {Tools = {"Level 5 Keycard", "P90", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5741882264, 5743709556}, HealthBonus = 200, TeamtagName = "Administrative Secretary"},
+	[10] = {Tools = {"Level 5 Keycard", "P90", "Glock 18", "Medkit", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5741882264, 5743709556}, HealthBonus = 250, TeamtagName = "Administrative Head of 51"},
+}
